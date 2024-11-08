87a6f3224e1f10474f085ecb60f87c85a237c863
Mahfuz <mahfuzur.rahman.swe@gmail.com>
2024-11-08 23:08:44 +0600

    updated path

---
+++ src/app/modules/landing/landing-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {LandingPageComponent} from "./feature/landing-page/landing-page.component";
+
+const routes: Routes = [
+  {path: 'mahfuzur-rahman', component: LandingPageComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class LandingRoutingModule { }
